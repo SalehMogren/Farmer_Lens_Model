8d46e9429fe18dcf3bc29091a300febe0157eaa3
SalehMogren <saleh0sm2@gmail.com>
2022-08-06 00:47:52 +0300

    beautify my code 😎

--- Model/predict_funciton/predict.py
+++ Model/predict_funciton/predict.py
@@ -7,39 +7,43 @@ import os
 from google.cloud import storage
 import functions_framework
 
-
-## Gloabl model variable
+# Gloabl model variable
 model = None
 
 # Download model file from cloud storage bucket
+
+
 def download_model_file():
 
     from google.cloud import storage
 
     # Model Bucket details
-    BUCKET_NAME        = os.environ['MODEL_BUCKET']
-    PROJECT_ID         = os.environ["GCP_PROJECT_ID"]
-    GCS_MODEL_FILE     = os.environ["MODEL_NAME"]
+    BUCKET_NAME = os.environ['MODEL_BUCKET']
+    PROJECT_ID = os.environ["GCP_PROJECT_ID"]
+    GCS_MODEL_FILE = os.environ["MODEL_NAME"]
 
     # Initialise a client
-    client   = storage.Client(PROJECT_ID)
-    
+    client = storage.Client(PROJECT_ID)
+
     # Create a bucket object for our bucket
-    bucket   = client.get_bucket(BUCKET_NAME)
-    
+    bucket = client.get_bucket(BUCKET_NAME)
+
     # Create a blob object from the filepath
-    blob     = bucket.blob(GCS_MODEL_FILE)
-    
+    blob = bucket.blob(GCS_MODEL_FILE)
+
     folder = '/tmp/'
     if not os.path.exists(folder):
-      os.makedirs(folder)
+        os.makedirs(folder)
     # Download the file to a destination
     blob.download_to_filename(folder + GCS_MODEL_FILE)
 
-def load_model(path:str):
-    model = tf.keras.models.load_model(path, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
+
+def load_model(path: str):
+    model = tf.keras.models.load_model(
+        path, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
     model.build((None, 224, 224, 3))
     return model
+
 
 def process_image(imageNp):
     #     Convert to TF
@@ -48,9 +52,10 @@ def process_image(imageNp):
     imageNp /= 255
     #     Convert to NP
     imageNp = imageNp.numpy()
-    return 
+    return
 
-def predict(image:Image,model):
+
+def predict(image: Image, model):
     test_image = np.asarray(image)
     processed_test_image = process_image(test_image)
     processed_test_image = np.expand_dims(processed_test_image, axis=0)
@@ -61,8 +66,8 @@ def predict(image:Image,model):
     classes = i.numpy().tolist()
     return probs, classes
 
-def download_image(image_path:str):
 
+def download_image(image_path: str):
     """Downloads a blob from the bucket."""
 
     storage_client = storage.Client()
@@ -72,6 +77,7 @@ def download_image(image_path:str):
     destinatoin = '/tmp/image.jpg'
     blob.download_to_filename(destinatoin)
     return destinatoin
+
 
 @functions_framework.http
 def predict_request(request):
@@ -89,20 +95,19 @@ def predict_request(request):
         download_model_file()
         model = load_model(f'/tmp/{os.environ["MODEL_NAME"]}')
 
-    # Get image from request 
+    # Get image from request
     params = request.get_json()
     if (params is not None) and ('img' in params):
         try:
-                
+
             # get image from storage bucket
             local_image_path = download_image(params['img'])
             img = Image.open(local_image_path)
 
             # get prediction
-            pred_type = predict(model=model,image=img)
-            return pred_type,200
+            pred_type = predict(model=model, image=img)
+            return pred_type, 200
         except Exception as e:
             return f'Error happend while proccessing the prediction Details : {e}'
     else:
-        return "couldn't proccess prediction",400
-
+        return "couldn't proccess prediction", 400
